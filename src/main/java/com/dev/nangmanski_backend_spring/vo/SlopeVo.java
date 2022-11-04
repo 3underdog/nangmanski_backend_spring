1cc8a99a4b0aef7c49ed237b89b7a26f21a1a646
SeungWookHan <hswook12@me.com>
2022-11-04 23:23:05 +0900

    add SlopeTimeVo list in SlopeVo
    Co-authored-by: chaLyoung <dreamlovinghigh57@gmail.com>

--- src/main/java/com/dev/nangmanski_backend_spring/vo/SlopeVo.java
+++ src/main/java/com/dev/nangmanski_backend_spring/vo/SlopeVo.java
@@ -5,6 +5,8 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @Data
 @AllArgsConstructor // 모든 필드값을 파라미터로 받는 생성자를 만듦
 @NoArgsConstructor //파라미터가 없는 기본 생성자를 생성
@@ -15,4 +17,5 @@ public class SlopeVo {
     private String slopeName;
     private String level;
     private char status;
+    private List<SlopeTimeVo> slopeTimeList;
 }
